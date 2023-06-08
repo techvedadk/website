c7377e5f9e9864a61310610114b7a8075e9aa5eb
shriharip <shrihari.p4@gmail.com>
2023-06-08 13:18:25 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,19 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  site: "https://techveda.netlify.app/",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
